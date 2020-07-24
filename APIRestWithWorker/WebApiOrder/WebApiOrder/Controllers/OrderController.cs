8733ce4512812af911e717c9293b2c94bf65cf4d
Thiago Cruz <thagocruz@gmail.com>
2020-07-24 11:59:12 -0300

    Add controller

---
+++ APIRestWithWorker/WebApiOrder/WebApiOrder/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using WebApiOrder.Domain;
+
+namespace WebApiOrder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult InsertOrder(Order order)
+        {
+            try
+            {
+                return Accepted(order);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error to insert order", ex);
+
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
