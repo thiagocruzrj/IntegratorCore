0956fe85c6c7ce49233fb8c6535a2b239af135f9
Thiago Cruz <thagocruz@gmail.com>
2020-07-24 11:46:41 -0300

    Add order entity

---
+++ APIRestWithWorker/WebApiOrder/WebApiOrder/Domain/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiOrder.Domain
+{
+    public sealed class Order
+    {
+        public int OrderNumber { get; set; }
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
