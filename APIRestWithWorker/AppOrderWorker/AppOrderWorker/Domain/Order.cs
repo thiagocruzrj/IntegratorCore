f1a8ebffc7e9843cef7c5d034e1b8481a5d9900e
Thiago Cruz <thagocruz@gmail.com>
2020-07-24 12:17:51 -0300

    Update Order.cs

--- APIRestWithWorker/AppOrderWorker/AppOrderWorker/Domain/Order.cs
+++ APIRestWithWorker/AppOrderWorker/AppOrderWorker/Domain/Order.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AppOrderWorker.Domain
+﻿namespace AppOrderWorker.Domain
 {
-    class Order
+    public sealed class Order
     {
+        public int OrderNumber { get; set; }
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
     }
 }
